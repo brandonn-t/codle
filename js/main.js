7d3f43d2e94056b34694ace404de9f144c5109f7
brandonn-t <brandon.torres619@gmail.com>
2022-08-08 16:03:48 -0400

    added functionality to the buttons

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
     createSquares();
 
+    const keys = document.querySelectorAll(".keyboardrow button");
+    let guessedWords = [[]]
+    let availableSpace = 1;
+
+    for (let index = 0; index < keys.length; index++) {
+        keys[index].onclick = ({ target }) => {
+            const letter = target.getAttribute("data-key");
+
+            updateGuessedWords(letter)
+        };        
+    }
+
+
+    function getCurrentWordArr(){
+        const numOfGuessedWords = guessedWords.length
+        return guessedWords[numOfGuessedWords - 1]
+    }
+    
+    function updateGuessedWords(letter){
+        const currentWordArr = getCurrentWordArr()
+        if (currentWordArr && currentWordArr.length < 5){
+            currentWordArr.push(letter)
+            const availableSpaceEl = document.getElementById(String(availableSpace))
+            availableSpace = availableSpace + 1
+
+            availableSpaceEl.textContent = letter;
+        }
+
+    }
+
+
 
     function createSquares() {
         const gameBoard = document.getElementById("board")
@@ -11,4 +42,6 @@ document.addEventListener("DOMContentLoaded", () => {
             gameBoard.appendChild(square);
         }
     }
+
+
 })
