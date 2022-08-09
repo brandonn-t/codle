e56151bb292da92bc7c6ebaa7f31f48765d54459
brandonn-t <brandon.torres619@gmail.com>
2022-08-09 17:41:55 -0400

    colors and animation added to words. Need to fix cases where user doesnt input 5 letters

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const keys = document.querySelectorAll(".keyboardrow button");
     let guessedWords = [[]]
     let availableSpace = 1;
-    let word = "dairy"
+    let word = "dairy";
+    let guessedWordCount = 0;
 
     function getCurrentWordArr(){
         const numOfGuessedWords = guessedWords.length
@@ -23,18 +24,55 @@ document.addEventListener("DOMContentLoaded", () => {
 
     }
 
+    function getTileColor(letter, index){
+        const isCorrectLetter = word.includes(letter);
+        if(!isCorrectLetter){
+            return "rgb(58, 58, 60)";
+        }
+
+        const letterInThatPosition = word.charAt(index);
+        const isCorrectPosition = letter === letterInThatPosition
+
+        if(isCorrectPosition){
+            return "rgb(83, 141, 78)";
+        }
+
+        return "rgb(181, 159, 59)";
+    }
+
+
     function handleSubmitWord(){
         const currentWordArr = getCurrentWordArr()
         if(currentWordArr.length !== 5){
             window.alert("Word must be 5 letters");
+            
+            
         }
 
-        const currentWord = currentWordArr.join('')
+        const currentWord = currentWordArr.join("");
+
+
+        const firstLetterId = guessedWordCount * 5 + 1;
+        const interval  = 250;
+
+        currentWordArr.forEach((letter, index) => {
+            setTimeout(() => {
+                const tileColor = getTileColor(letter, index)
+
+                const letterId = firstLetterId + index;
+                const letterEl = document.getElementById(letterId)
+                letterEl.classList.add("animate__flipInX");
+                letterEl.style = `background-color:${tileColor}; border-color:${tileColor}`
+            }, interval * index);
+        });
+
+        guessedWordCount += 1;
+
         if(currentWord === word){
             window.alert("Congratulations!");
         }
 
-        if(guessedWords.length === 6){
+        else if(guessedWords.length === 6){
             window.alert(`You lost :( , the word is ${word}.`);
         }
 
@@ -47,9 +85,20 @@ document.addEventListener("DOMContentLoaded", () => {
         for(let i = 0; i < 30; i++){
             let square = document.createElement("div")
             square.classList.add("square")
+            square.classList.add("animate__animated");
             square.setAttribute("id", i + 1)
             gameBoard.appendChild(square);
         }
+    }
+
+    function handleDeleteLetter(){
+        const currentWordArr = getCurrentWordArr()
+        const removedLetter = currentWordArr.pop()
+
+        guessedWords[guessedWords.length - 1] = currentWordArr;
+        const lastLetterEl = document.getElementById(String(availableSpace - 1))
+        lastLetterEl.textContent = '';
+        availableSpace = availableSpace - 1;
     }
 
     for (let index = 0; index < keys.length; index++) {
@@ -58,6 +107,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if(letter == 'enter'){
                 handleSubmitWord()
+                return;
+            }
+
+            if(letter == 'del'){
+                handleDeleteLetter();
                 return;
             }
 
